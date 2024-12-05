bf6b59a427cf32a3134b603be01e3553347e3661
Abdalla Hussein <AH1967@LIVE.MDX.AC.UK>
2024-12-06 02:30:47 +0400

    Add lessons API route

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Get all lessons');
+});
+
+module.exports = router;
+
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/after-school', {
     useNewUrlParser: true,
