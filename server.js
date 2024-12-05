92a006a6e46750839169126a948cec57b3573add
Abdalla Hussein <AH1967@LIVE.MDX.AC.UK>
2024-12-06 02:29:50 +0400

    Set up basic Express server

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/after-school', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+
+
 app.listen(3000, () => console.log('Server running on port 3000'));
